63c8af97e3f17e1553922a8a7d58f536db90a1d7
Max Huster <maxekinge>
2015-08-26 21:54:18 +0200

    Add Interface IMulti

---
+++ src/de/kryptondev/spacy/share/IMulti.java
@@ -0,0 +1,12 @@
+package de.kryptondev.spacy.share;
+
+/**
+* For multiple packages for one type of packet (class)
+* Example:      Position, Death, Chatmessage
+* 
+* No IMulti:    Version, ConnectionInfo
+*               This packages were sent just 1 time.
+*/
+public interface IMulti {
+    int packageId = 0;
+}
